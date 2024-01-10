fb960dc7b65716d194f6ed539bb4192c278e8d64
vood <balitsky_igor@list.ru>
2024-01-10 20:17:32 +0300

    init

--- src/main/kotlin/com/example/demodelete/controller/ProductController.kt
+++ src/main/kotlin/com/example/demodelete/controller/ProductController.kt
@@ -3,10 +3,12 @@ package com.example.demodelete.controller
 import com.example.demodelete.client.SomeClient
 import com.example.demodelete.dto.ApiDto
 import com.example.demodelete.dto.DataOk
+import com.example.demodelete.dto.Errr
 import kotlinx.serialization.json.Json
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -22,11 +24,14 @@ class ProductController(
     }
 
     @GetMapping("second/{id}")
-    suspend fun second(@PathVariable id: Int): String {
-        val apiDto = ApiDto(DataOk("$id second"))
-//        return apiDto
+    @ResponseBody
+    suspend fun second(@PathVariable id: Int): ApiDto {
+//        val apiDto = ApiDto(DataOk("$id second"))
+        val apiDto = ApiDto(Errr("$id second"))
+
         log.info("second -> " +j.encodeToString(ApiDto.serializer(), apiDto))
-        return j.encodeToString(ApiDto.serializer(), apiDto)
+//        return j.encodeToString(ApiDto.serializer(), apiDto)
+                return apiDto
     }
 
 }
