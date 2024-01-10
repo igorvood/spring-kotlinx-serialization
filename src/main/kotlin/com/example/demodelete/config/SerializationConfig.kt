56656629e650edb8128259555b8378859c316c5b
vood <balitsky_igor@list.ru>
2024-01-10 19:00:56 +0300

    init

--- src/main/kotlin/com/example/demodelete/config/SerializationConfig.kt
+++ src/main/kotlin/com/example/demodelete/config/SerializationConfig.kt
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.codec.json.KotlinSerializationJsonDecoder
 import org.springframework.http.codec.json.KotlinSerializationJsonEncoder
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 
 @Configuration
 class SerializationConfig {
