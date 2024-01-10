64a905b15ed4004bfd10af9450ec5e962be53251
vood <balitsky_igor@list.ru>
2024-01-10 18:33:28 +0300

    init

---
+++ src/main/kotlin/com/example/demodelete/DemoDeleteApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demodelete
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoDeleteApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoDeleteApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demodelete/DemoDeleteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demodelete
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoDeleteApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
