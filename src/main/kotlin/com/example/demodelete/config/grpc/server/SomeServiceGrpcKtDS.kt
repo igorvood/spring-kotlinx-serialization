ba56d36652f4bdf9a12d353718207e04cb46fc2e
vood <balitsky_igor@list.ru>
2024-01-14 14:41:23 +0300

    init

--- src/main/kotlin/com/example/demodelete/config/grpc/server/SomeServiceGrpcKtDS.kt
+++ src/main/kotlin/com/example/demodelete/config/grpc/server/SomeServiceGrpcKtDS.kt
@@ -1,28 +1,50 @@
 package com.example.demodelete.config.grpc.server
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.map
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import ru.vood.grpc.example.v1.RequestProtoDto
 import ru.vood.grpc.example.v1.ResponseProtoDto
 import ru.vood.grpc.example.v1.SomeServiceGrpcKt
+import kotlin.math.abs
 
 @Service
 class SomeServiceGrpcKtDS(val someServiceCoroutineClient: SomeServiceGrpcKt.SomeServiceCoroutineStub) : SomeServiceGrpcKt.SomeServiceCoroutineImplBase() {
-
+    private val log = LoggerFactory.getLogger(this.javaClass)
     override suspend fun execute(request: RequestProtoDto): ResponseProtoDto {
-        return super.execute(request)
+        log.info("execute: ${request.bar}")
+        return ResponseProtoDto.newBuilder()
+            .setStuff(request.bar+"-> BAR execute")
+            .build()
     }
 
     override fun executeStream(request: RequestProtoDto): Flow<ResponseProtoDto> {
-        return super.executeStream(request)
+        log.info("executeStream: ${request.bar}")
+        val cnt = abs(request.hashCode() % 10) + 2
+        val map = (1..cnt)
+            .asFlow()
+            .map { cnt ->
+
+                someServiceCoroutineClient.execute(
+                    RequestProtoDto.newBuilder()
+                        .setBar("$cnt BAR executeStream")
+                        .build()
+                )
+            }
+
+        return map
     }
 
 
     override suspend fun firstExecute(request: RequestProtoDto): ResponseProtoDto {
+        log.info("firstExecute: ${request.bar}")
         return someServiceCoroutineClient.secondExecute(request)
     }
 
     override suspend fun secondExecute(request: RequestProtoDto): ResponseProtoDto {
+        log.info("secondExecute: ${request.bar}")
         return ResponseProtoDto.newBuilder()
             .setStuff(request.bar+" secondExecute ")
             .build()
--- src/main/kotlin/com/example/demodelete/runner/GrpcRunner.kt
+++ src/main/kotlin/com/example/demodelete/runner/GrpcRunner.kt
@@ -1,6 +1,8 @@
 package com.example.demodelete.runner
 
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Service
 import ru.vood.grpc.example.v1.RequestProtoDto
@@ -8,18 +10,31 @@ import ru.vood.grpc.example.v1.SomeServiceGrpcKt
 
 @Service
 class GrpcRunner(val someServiceCoroutineStub: SomeServiceGrpcKt.SomeServiceCoroutineStub) : CommandLineRunner {
+    private val log = LoggerFactory.getLogger(this.javaClass)
     override fun run(vararg args: String?) {
+        log.info("===================Single begin==========================")
         runBlocking {
             val firstExecute = someServiceCoroutineStub.firstExecute(
                 RequestProtoDto.newBuilder()
-                    .setBar("Bar!!!!")
+                    .setBar("firstExecute Bar!!!!")
                     .build()
             )
-
-
-            println(firstExecute.stuff)
+            log.info(firstExecute.stuff)
+        }
+        log.info("===================Single end==========================")
+        log.info("===================Flow begin==========================")
+        runBlocking {
+            val firstExecute = someServiceCoroutineStub.executeStream(
+                RequestProtoDto.newBuilder()
+                    .setBar("executeStream Bar!!!!")
+                    .build()
+            )
+            firstExecute.map {f ->
+                log.info(f.stuff)
+            }
 
         }
+        log.info("===================Flow end==========================")
 
     }
 }
